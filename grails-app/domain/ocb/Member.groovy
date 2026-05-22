a535c1cd7d97a735ec5e56e6ec404e0aafbea2a1
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-05-22 14:54:14 +0530

    Member commit

---
+++ grails-app/domain/ocb/Member.groovy
@@ -0,0 +1,35 @@
+package ocb
+
+import com.grails.ocb.GlobalConfig
+
+class Member {
+
+    Integer id
+    String firstName
+    String lastName
+    String email
+    String password
+    String memberType = GlobalConfig.USER_TYPE.REGULAR_MEMBER
+    String identityHash
+    Date identityHashLastActive
+    Boolean isActive = true
+
+    Date dateCreated
+    Date lastUpdated
+
+    static constraints = {
+        email(email: true, nullable: false, unique: true, blank: false)
+        password(blank: false)
+        lastName(nullable: true)
+        identityHash(nullable: true)
+        identityHashLastActive(nullable: true)
+    }
+
+    def beforeInsert(){
+        this.password = this.password.encodeAsMD5()
+    }
+
+    def afterInsert(){
+        this.password = this.password.encodeAsMD5()
+    }
+}
