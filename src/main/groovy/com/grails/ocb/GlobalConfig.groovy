a48f580d9cf5de0c8914016547dcfa7ec1c717ae
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-05-22 14:34:19 +0530

    main commit 1

---
+++ src/main/groovy/com/grails/ocb/GlobalConfig.groovy
@@ -0,0 +1,9 @@
+package com.grails.ocb
+
+class GlobalConfig {
+
+    private static final def USER_TYPE=[
+        ADMINISTRATOR:"ADMINISTRATOR",
+        REGULAR_MEMBER:"REGULAR MEMBER",
+    ]
+}
